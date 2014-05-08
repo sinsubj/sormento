757b710fdf2f01e745ec4f4df60f85d3f26693ae
kenju254 <kenmbugua@gmail.com>
2014-05-08 22:57:00 +0300

    Fixed static files issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,11 +97,10 @@ DEFAULT_CHARSET = 'utf-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = STATIC_PATH
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     STATIC_PATH,
-# )
+STATICFILES_DIRS = (
+     STATIC_PATH,
+)
 
 
 # Media files
